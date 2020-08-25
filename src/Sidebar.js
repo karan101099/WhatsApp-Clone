f539e5d64ec6af23d7318549e8328c145ac51520
Karan <karan101099@gmail.com>
2020-08-26 03:16:53 +0530

    before messages

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -7,9 +7,12 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
 import "./Sidebar.css";
 import db from "./firebase";
+import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
   const [rooms, setRooms] = useState([]);
+  const [{ user }, dispatch] = useStateValue();
+
   useEffect(() => {
     db.collection("rooms").onSnapshot((snapshot) =>
       setRooms(
@@ -24,7 +27,7 @@ function Sidebar() {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar></Avatar>
+        <Avatar src={user?.photoURL}></Avatar>
         <div className="sidebar__headerRight">
           <IconButton>
             <DonutLargeIcon />
